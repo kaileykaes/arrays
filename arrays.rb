3499bd2f79a45e4350cad9533c3bbe840f3c7705
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2022-12-07 16:00:11 -0700

    Declare four variables storing arrays

---
+++ arrays.rb
@@ -0,0 +1,5 @@
+composers = ['bach', 'fkj', 'handel', 'mvula', 'coleridge-taylor', 'bonds']
+integers = [2, 3, 5, 1, 9, 10]
+constants = [3.1415, 2.1782, 1.1412, 1.7320, 2.5029, 4.6692]
+for_george_boole = [true, false, false, false, true, false, true, true]
+
